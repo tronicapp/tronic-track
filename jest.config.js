8885939247d8b7a7ae381ebeea249cc014ee2e24
React <ken_mccloskey@hotmail.com>
2023-11-17 09:52:11 -0500

    Adds initial jest support

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const { createJestTSConfig } = require('@internal/config')
+
+/**
+ * This config detects our monorepo, and allows
+ * you to run every single package test in a single jest instance using 'yarn jest'.
+ * Thus, `yarn jest --watch` works as expected (unlike say, when using turborepo)
+ */
+module.exports = () =>
+  createJestTSConfig(__dirname, {
+    projects: [
+      // being explicit here, as a globbing bug means that using packages/* can cause problems in non-worktrees (haven't fully investigated yet).
+      '<rootDir>/packages/core',
+      // '<rootDir>/packages/core-integration-tests',
+      '<rootDir>/packages/node',
+      '<rootDir>/packages/browser',
+      // '<rootDir>/packages/consent/consent-tools',
+      // '<rootDir>/packages/consent/consent-wrapper-onetrust',
+    ],
+  })
